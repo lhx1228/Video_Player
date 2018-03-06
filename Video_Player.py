e8c33e6ddba6638df931439e44a863fe59d4bcbf
lhx1228 <1040115517@qq.com>
2018-03-06 10:32:08 +0800

    视频播放器+VIP解析

---
+++ Video_Player.py
@@ -0,0 +1,37 @@
+import tkinter
+import webbrowser
+from tkinter import *
+from tkinter import filedialog
+
+
+textLyric = ''
+e1 = ''
+
+def main(Video_url):
+    #Video_url = sys.argv[1]
+
+    url = 'http://api.3ewl.cc/qingfeng.php?url={}'.format(Video_url)
+
+    webbrowser.open(url)
+
+def adjust():
+    #print(e1.get())
+    main(e1.get())
+
+def Input_url():
+    global e1
+    frame = tkinter.Frame(master,height = 300,width = 500,bg = "#FFFFFF").pack()
+    #frame.pack(side=tkinter.LEFT, fill=tkinter.Y)
+    e1 = Entry(frame, bg='#FFFFFF')
+    e1.place(x=140, y=100)
+    Word = e1.get()
+    tkinter.Label(master, text="输入", fg='#000000',
+                  bg="#FFFFFF").place(x=100, y=98)
+    Button(master, text='播放',command=adjust).place(x=180, y=130)
+    
+if __name__ == '__main__':
+    master = tkinter.Tk()
+    master.title('GoogleTranslate')
+    master.geometry("400x300+200+100")
+    Input_url()
+    master.mainloop()
