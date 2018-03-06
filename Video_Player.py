47d30d57c4f4bb138831f06cfb8747e07de528d9
lhx1228 <1040115517@qq.com>
2018-03-06 11:21:19 +0800

    视频播放+VIP解析

--- Video_Player.py
+++ Video_Player.py
@@ -4,13 +4,21 @@ from tkinter import *
 from tkinter import filedialog
 
 
-textLyric = ''
+jiexi_url = ''
 e1 = ''
+
+def VIP():
+    global jiexi_url
+    jiexi_url = 'http://api.3ewl.cc/qingfeng.php?url'
+
+def Not_VIP():
+    global jiexi_url
+    jiexi_url = 'http://www.wmxz.wang/video.php?url'
 
 def main(Video_url):
     #Video_url = sys.argv[1]
 
-    url = 'http://api.3ewl.cc/qingfeng.php?url={}'.format(Video_url)
+    url = '{}={}'.format(jiexi_url,Video_url)
 
     webbrowser.open(url)
 
@@ -28,7 +36,11 @@ def Input_url():
     tkinter.Label(master, text="输入", fg='#000000',
                   bg="#FFFFFF").place(x=100, y=98)
     Button(master, text='播放',command=adjust).place(x=180, y=130)
-    
+    #单选按钮
+    v = IntVar()
+    R_ONE=Radiobutton(master, text="VIP", variable=v, value=1,bg = "#FFFFFF",command=VIP).place(x=230,y=50)
+    R_TWO=Radiobutton(master, text="Not VIP", variable=v, value=2,bg = "#FFFFFF",command=Not_VIP).place(x=140,y=50)
+
 if __name__ == '__main__':
     master = tkinter.Tk()
     master.title('VideoPlayer')
